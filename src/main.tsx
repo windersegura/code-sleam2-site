dbde01b1ccd40c03ada48970a445a298c21fa7f0
winders98 <segurawinder@gmail.com>
2025-10-25 16:58:37 -0600

    add login page

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import Home from './pages/home/Home.tsx'
+import Login from './pages/login/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />}/>
+      <Route path='/login' element={<Login />}/>
     </Routes>
   </BrowserRouter>,
 )
