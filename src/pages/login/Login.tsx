0ab9e85d1c8295142b911e5d70cf13286864730a
winders98 <segurawinder@gmail.com>
2025-11-06 23:56:50 -0600

    working in login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,24 @@
+import './login.css'
+
 export default function Login () {
 
     return (
-        <h1>login</h1>
+        <>
+            <main>
+                <form action="" className="login-form">
+                    <div className="login-form-items">
+                        <h1>Login</h1>
+                        <label>Correo Electronico</label>
+                        <input type="text" placeholder="Correo Electronico"/>
+
+                        <label>Contraseña</label>
+                        <input type="text" placeholder="Contraseña" />
+
+                        <button>Iniciar Sesion</button>
+                    </div>
+                </form>
+            </main>
+        </>
     )
 }
 
